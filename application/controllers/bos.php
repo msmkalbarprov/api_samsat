4a2ba34fcfd0833c1e8f390c8889c9e2df40fd46
msmkalbarprov <msmkalbar.prov@gmail.com>
2023-07-21 13:53:50 +0700

    add helper

--- application/controllers/bos.php
+++ application/controllers/bos.php
@@ -26,7 +26,7 @@ class Bos extends REST_Controller
 		parent::__construct();
 		$this->__getMyMethods();
 		$this->load->model('bosModel');
-        $this->load->helper('helper');
+        $this->load->helper();
 	}
     
     function view_get(){
