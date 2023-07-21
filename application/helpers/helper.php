361b5fb9d341fd8beadaa149248bea77e6a7ed43
msmkalbarprov <msmkalbar.prov@gmail.com>
2023-07-21 13:51:20 +0700

    add api bos

---
+++ application/helpers/helper.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+    
+    // -----------------------------------------------------------------------------
+    //check auth
+    if (!function_exists('getallheaders')) {
+        function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+        }
+    }
+
+
+?>
